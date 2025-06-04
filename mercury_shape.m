053acb44df7152afab5d5083cca23a825653aca7
thilo <thilo@MacBook-Air-von-Thilo.local>
2025-06-04 17:15:01 +0200

    added Viuguer_Anomaly file and added the Bouguer Correction to mercury_shape file

--- mercury_shape.m
+++ mercury_shape.m
@@ -3,10 +3,13 @@ clear
 close all
 clc
 
+%% constants
+G = 6.67430e-11;
+rho_crust = 2800;
+
 %% load data
 % load('Data/acton.mat')
 data = read(Tiff('Mercury_Messenger_USGS_DEM_Global_665m_v2.tif'));
-data2 = read(Tiff('MercuryMessengerUSGS_MAP2_EQUI.tif'));
 
 %% processing
 resolution = 64;
@@ -16,12 +19,19 @@ longitudes = -180 : 1/resolution : 180;
 longitudes = longitudes(1:end-1);
 
 multiplier = 0.5;
-elevations = multiplier * data;
+elevations = multiplier * double(data);
+
+%% calculate Bouguer anomaly
+deltag_b = 2*pi*G*rho_crust*elevations;
 
 %% plotting
-imagesc(longitudes, latitudes, elevations)
-colorbar
+% topography = elevations/1000;
+% imagesc(longitudes, latitudes, topography)
+% colorbar
 % colormap(acton)
+
+imagesc(longitudes, latitudes, deltag_b)
+colorbar
 
 %% analysis
 disp(max(max(data./2)))
